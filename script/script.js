c73ab92a741c7fcb2ec70b7471b767bd11f3df1b
Dylan Balvidares <Dylanaza037@gmail.com>
2025-11-25 01:29:02 -0300

    web v3

--- script/script.js
+++ script/script.js
@@ -1,24 +1,29 @@
-// Variables globales simples
 var modal = document.getElementById('miModal');
 var titulo = document.getElementById('tituloModal');
 var desc = document.getElementById('descModal');
 var link = document.getElementById('linkDescarga');
+var img = document.getElementById('imgModal');
 
-// Funcion para abrir el modal (llamada desde el HTML onclick)
-function abrirModal(nombreJuego, descripcion, rutaJar) {
+function abrirModal(nombreJuego, descripcion, rutaJar, rutaImagen) {
     titulo.innerText = nombreJuego;
     desc.innerText = descripcion;
-    link.href = rutaJar; // Pone el link al jar
+    link.href = rutaJar;
 
-    modal.style.display = 'flex'; // Muestra la caja
+    if (rutaImagen) {
+        img.src = rutaImagen;
+        img.style.display = 'block';
+    } else {
+        img.style.display = 'none';
+    }
+
+    modal.style.display = 'flex';
 }
 
-// Funcion para cerrar
 function cerrarModal() {
     modal.style.display = 'none';
+    img.src = ''; // limpiar para que no parpadee la anterior
 }
 
-// Cerrar si clic afuera de la caja
 window.onclick = function (event) {
     if (event.target == modal) {
         cerrarModal();
